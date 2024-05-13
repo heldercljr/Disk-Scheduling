badfc4fe52cbedd6456b6c9e17a17b221ca2a381
Helder-JR <helder.junior@ccc.ufcg.edu.br>
2024-05-13 11:54:46 -0300

    Remoção de variáveis desnecessárias.

--- algorithms/fcfs/fcfs.c
+++ algorithms/fcfs/fcfs.c
@@ -2,9 +2,6 @@
 
 void fcfs(Request* requests, uint requests_amount, Request current_request, Disk disk, FILE* report) {
 
-	uint current_sector = current_request.sector;
-	uint current_track = current_request.track;
-
 	double total_seek_time = 0;
 	double total_rotation_time = 0;
 	double total_transfer_time = 0;
@@ -14,15 +11,15 @@ void fcfs(Request* requests, uint requests_amount, Request current_request, Disk
 
 	for (uint index = 0; index < requests_amount; index++) {
 
-		total_seek_time += calculate_seek_time(current_track, requests[index].track, disk.seek_time);
-		total_rotation_time += calculate_rotation_time(current_sector, requests[index].sector, disk.sectors_per_track, disk.rotation_time);
+		total_seek_time += calculate_seek_time(current_request.track, requests[index].track, disk.seek_time);
+		total_rotation_time += calculate_rotation_time(current_request.sector, requests[index].sector, disk.sectors_per_track, disk.rotation_time);
 		total_transfer_time += calculate_transfer_time(1, disk.sector_size, disk.transfer_rate);
 		total_io_time = total_seek_time + total_rotation_time + total_transfer_time;
 
-		fprintf(report, "%u,%u,%u,%.3f,%.3f,%.3f,%.3f\n",index, current_sector, current_track, total_seek_time, total_rotation_time, total_transfer_time,total_io_time);
+		fprintf(report, "%u,%u,%u,%.3f,%.3f,%.3f,%.3f\n",index, current_request.sector, current_request.track, total_seek_time, total_rotation_time, total_transfer_time,total_io_time);
 
-		current_sector = requests[index].sector;
-		current_track = requests[index].track;
+		current_request.sector = requests[index].sector;
+		current_request.track = requests[index].track;
 
 		requests[index].served = True;
 	}
--- algorithms/sstf/sstf.c
+++ algorithms/sstf/sstf.c
@@ -2,9 +2,6 @@
 
 void sstf(Request* requests, uint requests_amount, Request current_request, Disk disk, FILE* report) {
 
-	uint current_sector = current_request.sector;
-	uint current_track = current_request.track;
-
 	double total_seek_time = 0;
 	double total_rotation_time = 0;
 	double total_transfer_time = 0;
@@ -22,7 +19,7 @@ void sstf(Request* requests, uint requests_amount, Request current_request, Disk
 
 			if (!requests[index].served) {
 
-				uint tracks_difference = abs(current_track - requests[index].track);
+				uint tracks_difference = abs(current_request.track - requests[index].track);
 
 				if (tracks_difference < minimum_difference) {
 
@@ -33,15 +30,15 @@ void sstf(Request* requests, uint requests_amount, Request current_request, Disk
 			}
 		}
 
-		total_seek_time += calculate_seek_time(current_track, requests[minimum_index].track, disk.seek_time);
-		total_rotation_time += calculate_rotation_time(current_sector, requests[minimum_index].sector, disk.sectors_per_track, disk.rotation_time);
+		total_seek_time += calculate_seek_time(current_request.track, requests[minimum_index].track, disk.seek_time);
+		total_rotation_time += calculate_rotation_time(current_request.sector, requests[minimum_index].sector, disk.sectors_per_track, disk.rotation_time);
 		total_transfer_time += calculate_transfer_time(1, disk.sector_size, disk.transfer_rate);
 		total_io_time = total_seek_time + total_rotation_time + total_transfer_time;
 
-		fprintf(report, "%u,%u,%u,%.3f,%.3f,%.3f,%.3f\n",step, current_sector, current_track, total_seek_time, total_rotation_time, total_transfer_time,total_io_time);
+		fprintf(report, "%u,%u,%u,%.3f,%.3f,%.3f,%.3f\n",step, current_request.sector, current_request.track, total_seek_time, total_rotation_time, total_transfer_time,total_io_time);
 
-		current_sector = requests[minimum_index].sector;
-		current_track = requests[minimum_index].track;
+		current_request.sector = requests[minimum_index].sector;
+		current_request.track = requests[minimum_index].track;
 
 		requests[minimum_index].served = True;
 	}
